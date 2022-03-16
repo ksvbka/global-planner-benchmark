579421c9bf34f9ff146a9e6cbf903564106d6841
Trung Kien <letrungkien.k53.hut@gmail.com>
2022-03-16 10:21:50 +0700

    Init project

---
+++ include/planner_benchmark.h
@@ -0,0 +1,6 @@
+#ifndef __PLANNER_BENCHMARK_H__
+#define __PLANNER_BENCHMARK_H__
+
+#include <ros/ros.h>
+
+#endif //__PLANNER_BENCHMARK_H__
---
+++ src/planner_benchmark.cpp
@@ -0,0 +1,10 @@
+#include <planner_benchmark.h>
+
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "planner_benchmark_node");
+  ROS_INFO("Hello world");
+
+  ros::spin();
+
+  return 0;
+}
